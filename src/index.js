43e4cb209367709b6fb533f31d5f3138f3b2d6ac
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-07-02 11:34:46 +0300

    feat: done Morse code decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,40 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    let arrNumber = [];
+    let arrDotsDashes = [];
+    let stringWords = [];
+  
+    for (let i = 0; i < expr.length; i+= 10) {
+      arrNumber.push(expr.slice(i, i+10));
+    }
+  
+    for (let i = 0; i < arrNumber.length; i++) {
+      arrDotsDashes.push([]);
+      for (let j = 0; j < arrNumber[i].length; j += 2) {
+        if (arrNumber[i][j] == 1 && arrNumber[i][j + 1]  == 1) {
+          arrDotsDashes[i].push('-');
+        } else if (arrNumber[i][j] == 1 && arrNumber[i][j + 1]  == 0) {
+          arrDotsDashes[i].push('.');
+        } else if (arrNumber[i][j] == '*' && arrNumber[i][j + 1]) {
+          arrDotsDashes[i].push(' ');
+          j = 10;
+        }
+      }
+    }
+  
+    for (let i = 0; i < arrDotsDashes.length; i++) {
+      arrDotsDashes[i] = arrDotsDashes[i].join('');
+      arrDotsDashes[i] = MORSE_TABLE[arrDotsDashes[i]];
+    }
+    
+    stringWords = arrDotsDashes.join('');
+    return stringWords;
 }
 
 module.exports = {
